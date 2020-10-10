d576e7e53229c0025a535a2549d2a5dc1ea62679
Robin De Schepper <robin.deschepper93@gmail.com>
2020-10-10 10:16:49 +0200

    Made debug mode configurable

--- vault/settings.py
+++ vault/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", False) == "TRUE"
 
 ALLOWED_HOSTS = [s for s in os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",") if s != ""]
 
